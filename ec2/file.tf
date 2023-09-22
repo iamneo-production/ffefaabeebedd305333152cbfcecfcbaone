3b12bf4370012257b3cf1b2ebe7b94e27c0eb95b
Iamneo <techadmin@iamneo.ai>
2023-09-22 16:39:27 +0000

    Backup Commit

--- ec2/file.tf
+++ ec2/file.tf
@@ -1,6 +1,12 @@
 provider "aws" {
     region = "ap-northeast-1"
-    access_key = ""
-    secret_key = ""
+    access_key = "AKIA2EEQ6TYPEWQRXCU3"
+    secret_key = "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"
 }
-resource ""
+resource "aws_instance" "new_instance" {
+    ami = "ami-0ed99df77a82560e6 "
+    instance_type = "t2.micro"
+}
+output "public_ip" {
+    value = aws_instance.new_instance.public_ip
+}
